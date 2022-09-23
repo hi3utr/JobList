7c31fca5025fc4f78bd0bc714bcda13b424ee748
hi3utr <hieu@email.com>
2022-09-23 17:57:07 +0700

    feature: add delete feature

--- src/App.js
+++ src/App.js
@@ -6,18 +6,26 @@ import axios from "axios";
 
 function App() {
   const [jobs, setJobs] = useState([]);
+  const [deleteJob, setDeleteJob] = useState(false);
+  var jobApi = "https://630eca933792563418817e08.mockapi.io/products";
   useEffect(() => {
-    axios
-      .get(`https://630eca933792563418817e08.mockapi.io/products`)
-      .then((res) => {
-        setJobs(res.data);
-      });
-  }, []);
+    axios.get(jobApi).then((res) => {
+      setJobs(res.data);
+    });
+  }, [deleteJob]);
 
   return (
     <div className="App">
       <NavBar />
-      {jobs.length > 0 && <TableList jobs={jobs} />}
+      {jobs.length > 0 && (
+        <TableList
+          jobs={jobs}
+          setJobs={setJobs}
+          jobApi={jobApi}
+          deleteJob={deleteJob}
+          setDeleteJob={setDeleteJob}
+        />
+      )}
     </div>
   );
 }
--- src/TableList.js
+++ src/TableList.js
@@ -1,30 +1,29 @@
-import { Space, Table, Tag, Button, Modal } from "antd";
+import { Space, Table, Tag, Modal, Form, Select, DatePicker } from "antd";
 import { ExclamationCircleOutlined } from "@ant-design/icons";
-import React from "react";
+import React, { useState } from "react";
 import * as dayjs from "dayjs";
+import Item from "antd/lib/list/Item";
 
 const TableList = (props) => {
+  const [isEditModalOpen, setIsEditModalOpen] = useState(false);
+  const onChange = (date, dateString) => {
+    console.log(date, dateString);
+  };
+  const showEditModal = () => {
+    setIsEditModalOpen(true);
+  };
+
+  const handleOk = () => {
+    setIsEditModalOpen(false);
+  };
+
+  const handleCancel = () => {
+    setIsEditModalOpen(false);
+  };
   const { confirm } = Modal;
 
   const tags = ["done", "doing", "fail"];
-  const showDeleteConfirm = () => {
-    confirm({
-      title: "Are you sure delete this task?",
-      icon: <ExclamationCircleOutlined />,
-      content: "Some descriptions",
-      okText: "Yes",
-      okType: "danger",
-      cancelText: "No",
 
-      onOk() {
-        console.log("OK");
-      },
-
-      onCancel() {
-        console.log("Cancel");
-      },
-    });
-  };
   const columns = [
     {
       title: "NAME",
@@ -70,40 +69,95 @@ const TableList = (props) => {
       key: "action",
       render: (_, record) => (
         <Space size="middle">
-          <button>
+          <button onClick={showEditModal}>
             <img src="assets/icons/edit.png" alt="" />
           </button>
-          <button onClick={showDeleteConfirm}>
+
+          <button
+            onClick={() => {
+              showDeleteConfirm(record);
+            }}
+          >
             <img src="assets/icons/delete.png" alt="" />
           </button>
         </Space>
       ),
     },
   ];
-  const data = [
-    {
-      key: "1",
-      name: "Hieu",
-      created: "",
-      deadline: "",
-      tags: ["done"],
-    },
-    {
-      key: "2",
-      name: "",
-      created: "",
-      deadline: "",
-      tags: ["fail"],
-    },
-    {
-      key: "3",
-      name: "",
-      created: "",
-      deadline: "",
-      tags: ["doing"],
-    },
-  ];
-  return <Table columns={columns} dataSource={props.jobs} />;
+
+  const handleDelete = (record) => {
+    props.setJobs((prev) => {
+      return prev.filter((job) => job.key !== record.key);
+    });
+  };
+  function handleDeleteCourse(record) {
+    var options = {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    fetch(props.jobApi + "/" + record.key, options)
+      .then((response) => {
+        response.json();
+      })
+      .then(function () {});
+  }
+  const showDeleteConfirm = (record) => {
+    confirm({
+      title: "Are you sure delete this task?",
+      icon: <ExclamationCircleOutlined />,
+      content: "This action cannot be undone",
+      okText: "Yes",
+      okType: "danger",
+      cancelText: "No",
+
+      onOk() {
+        // handleDelete(record);
+        handleDeleteCourse(record);
+        props.setDeleteJob(!props.deleteJob);
+      },
+      onCancel() {
+        console.log("Cancel");
+      },
+    });
+  };
+  return (
+    <div>
+      <Table columns={columns} dataSource={props.jobs} />
+      <Modal
+        title="EDIT TASK"
+        open={isEditModalOpen}
+        onOk={handleOk}
+        onCancel={handleCancel}
+      >
+        <div className="mb-[12px]">
+          <p>Task name</p>
+          <input
+            className="border rounded-[5px] w-full px-[15px] py-[8px]"
+            placeholder="Task name"
+            type="text"
+          />
+        </div>
+        <div className="mb-[12px]">
+          <p>Deadline</p>
+          <Space style={{ width: "100%" }} direction="vertical">
+            <DatePicker onChange={onChange} />
+          </Space>
+        </div>
+        <div>
+          <p>Status</p>
+          <Form.Item>
+            <Select>
+              <Select.Option value="done">Done</Select.Option>
+              <Select.Option value="doing">Doing</Select.Option>
+              <Select.Option value="fail">Fail</Select.Option>
+            </Select>
+          </Form.Item>
+        </div>
+      </Modal>
+    </div>
+  );
 };
 
 export default TableList;
